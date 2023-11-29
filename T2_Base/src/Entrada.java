5c9a0cb853caeceea92f3e0423946e744fb6624b
chitodreco <148772347+chitodreco@users.noreply.github.com>
2023-11-29 20:33:08 +0100

    update

---
+++ T2_Base/src/Entrada.java
@@ -0,0 +1,8 @@
+public class Entrada {
+
+    public static void main(String[] args) {
+        double radio = 4.5;
+        double area = 2*Math.PI*Math.pow(radio,2);
+        System.out.println("El area del circulo es "+area);
+    }
+}
