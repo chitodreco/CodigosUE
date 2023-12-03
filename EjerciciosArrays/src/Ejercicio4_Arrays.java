2822d79eb70e970bab4505adcf89f2ff7437b44c
chitodreco <148772347+chitodreco@users.noreply.github.com>
2023-12-03 21:33:58 +0100

    upload

--- EjerciciosArrays/src/Ejercicio4_Arrays.java
+++ EjerciciosArrays/src/Ejercicio4_Arrays.java
@@ -2,7 +2,7 @@
 // Carga el array numero con valores aleatorios entre 0 y 100.
 //        - En el array cuadrado se deben almacenar los cuadrados de los valores que hay en el array numero.
 //        - En el array cubo se deben almacenar los cubos de los valores que hay en numero.
-
+// Una ver realizado lo anterior muestra el contenido de los tres arrays dispuesto en tres columnas.
 
 public class Ejercicio4_Arrays {
 
@@ -16,27 +16,19 @@ public class Ejercicio4_Arrays {
             int aleatorio = (int) (Math.random()*100);
             numero[i] = aleatorio;
         }
-        for (int item : numero) {
-            System.out.print(item + " ");
-        }
-
-        System.out.println();
 
         for (int i = 0; i < cuadrado.length; i++) {
             cuadrado[i] = (int) Math.pow(numero[i], 2);
         }
-        for (int item : cuadrado) {
-            System.out.print(item + " ");
-        }
-
-        System.out.println();
 
         for (int i = 0; i < cubo.length; i++) {
             cubo[i] = (int) Math.pow(numero[i], 3);
         }
-        for (int item : cubo) {
-            System.out.print(item + " ");
+
+        for (int i = 0; i < numero.length; i++) {
+            System.out.println(numero[i] + " " + cuadrado[i] + " " + cubo[i]);
         }
+
     }
 
 }
