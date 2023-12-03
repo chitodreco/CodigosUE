bd4991f81512f094934bdaac3ee0d65456bd687a
chitodreco <148772347+chitodreco@users.noreply.github.com>
2023-12-03 22:40:06 +0100

    upload

---
+++ EjerciciosArrays/src/Ejercicio7_Arrays.java
@@ -0,0 +1,44 @@
+//1. Crea un programa que pida 10 palabras por consola y las guarde en un array de String.
+// Una vez hecho esto saldrá un menú que me dará las siguientes opciones:
+//        - Ver todas las palabras: mostrarla todas las palabras del array
+//        - Obtener una palabra al azar: mostrará una palabra de las que existen en el array
+//        - Ver número de letras: mostrará el número de letras que hay en total entre todas las palabras
+//        - Ver media de letras: mostrará el número medio de letras entre todas las palabas
+//        - Ver palabra con más letras
+//        - Ver palabra con menos letras
+
+import java.util.Scanner;
+
+public class Ejercicio7_Arrays {
+
+    public static void main(String[] args) {
+
+        String[] palabras = new String[10];
+        Scanner scanner = new Scanner(System.in);
+
+
+        for (int i = 0; i < palabras.length; i++) {
+            System.out.println("Introduzca una palabra: ");
+            String palabra = scanner.next();
+            palabras[i] = palabra;
+        }
+        System.out.println();
+        for (String item: palabras) {
+            System.out.print(item + " ");
+        }
+        System.out.println();
+
+        int aleatorio = (int) (Math.random()*10);
+        for (String item:palabras) {
+            System.out.println(palabras[aleatorio]);
+            break;
+        }
+
+        System.out.println();
+
+        for (int i = 0; i < palabras.length; i++) {
+            palabras[i] = 
+        }
+
+    }
+}
