9fa0b2f46ecf40afbbd175a2e387d6c91fc5c943
chitodreco <148772347+chitodreco@users.noreply.github.com>
2023-12-05 19:32:30 +0100

    upload

---
+++ EjerciciosArraysMultid_Unid/src/Ejercicio2_ArrayMulti.java
@@ -0,0 +1,15 @@
+//2. (MayorArray) Crea una aplicación que realice lo siguiente:
+//        - pida por pantalla la longitud del array de enteros
+//        - crea el array de enteros con la longitud introducida
+//        - pida al usuario que introduzca todos los datos del array
+//        - saque por pantalla el elemento mayor y el elemento menor
+
+public class Ejercicio2_ArrayMulti {
+
+    public static void main(String[] args) {
+
+        
+
+    }
+
+}
