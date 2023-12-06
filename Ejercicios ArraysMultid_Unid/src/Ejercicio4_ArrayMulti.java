3d2115c9c9596d6229bb22299efcf4567cd0e158
chitodreco <148772347+chitodreco@users.noreply.github.com>
2023-12-06 17:34:55 +0100

    update

---
+++ Ejercicios ArraysMultid_Unid/src/Ejercicio4_ArrayMulti.java
@@ -0,0 +1,14 @@
+//4. (PosicionesArray) Crea una aplicación que:
+//        - pida por teclado la longitud de un array
+//        - cree el array de enteros de la longitud introducida
+//        - pida al usuario que introduzca todos los datos del array
+//        - mueva una posición todos los elementos. el elemento de la posición 0 pasa a la posición 1
+//        y así sucesivamente. El elemento que está en la última posición pasa a la primera
+//        - realizar la tarea anterior pero al contrario. El elemento que está en la primera posición
+//        pasa a la última y así sucesivamente
+
+public class Ejercicio4_ArrayMulti {
+
+    
+
+}
