2d5efe96170d5bc4de78b1149c0a0c7f4cf786a7
chitodreco <148772347+chitodreco@users.noreply.github.com>
2023-12-06 17:04:05 +0100

    update

---
+++ EjerciciosArraysMultid_Unid/src/Ejercicio3_ArrayMulti.java
@@ -0,0 +1,17 @@
+//3. (SumaArrays) Crear una aplicación que:
+//        - pida la longitud de dos arrays de enteros
+//        - crea los array de enteros con las longitudes introducidas
+//        - crea un tercer array de la misma longitud que los dos anteriores
+//        - rellena el tercer array con la suma de las posiciones
+//        (Array1.posicion1 + array2.posicion = array3.posicion)
+
+
+public class Ejercicio3_ArrayMulti {
+
+    public static void main(String[] args) {
+
+        
+
+    }
+
+}
