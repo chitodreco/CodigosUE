1f24dfd1d33f8949dca6d68fee999a44271b52b7
chitodreco <148772347+chitodreco@users.noreply.github.com>
2023-12-06 17:32:50 +0100

    update

--- EjerciciosArraysMultid_Unid/src/Ejercicio3_ArrayMulti.java
+++ EjerciciosArraysMultid_Unid/src/Ejercicio3_ArrayMulti.java
@@ -6,11 +6,42 @@
 //        (Array1.posicion1 + array2.posicion = array3.posicion)
 
 
+import java.util.Scanner;
+
 public class Ejercicio3_ArrayMulti {
 
     public static void main(String[] args) {
 
-        
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Introduzca longitud para 2 arrays: ");
+        int longitud1 = scanner.nextInt();
+        int [] array1 = new int[longitud1];
+        int[] array2 = new int[longitud1];
+
+        for (int i = 0; i < array1.length; i++) {
+            array1[i] = (int) (Math.random()*99);
+        }
+        for (int item:array1) {
+            System.out.print(item + " ");
+        }
+        System.out.println();
+        for (int i = 0; i < array2.length; i++) {
+            array2[i] = (int) (Math.random()*99);
+        }
+        for (int item:array2) {
+            System.out.print(item + " ");
+        }
+        System.out.println();
+
+        int [] array3 = new int[array2.length];
+
+        for (int i = 0; i < array3.length; i++) {
+            array3[i] = array2[i] + array1[i];
+        }
+        for (int item:array3) {
+            System.out.print(item + " ");
+        }
 
     }
 
