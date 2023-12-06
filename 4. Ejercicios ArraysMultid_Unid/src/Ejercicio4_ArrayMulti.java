7f933166e8eecc9de10a6ee94c391a98842615a9
chitodreco <148772347+chitodreco@users.noreply.github.com>
2023-12-06 19:50:52 +0100

    update

--- 4. Ejercicios ArraysMultid_Unid/src/Ejercicio4_ArrayMulti.java
+++ 4. Ejercicios ArraysMultid_Unid/src/Ejercicio4_ArrayMulti.java
@@ -7,8 +7,39 @@
 //        - realizar la tarea anterior pero al contrario. El elemento que está en la primera posición
 //        pasa a la última y así sucesivamente
 
+import java.util.Scanner;
+
 public class Ejercicio4_ArrayMulti {
 
-    
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduzca longitud del array: ");
+        int longitud = scanner.nextInt();
+        int [] array = new int[longitud];
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println("Introduzca número en el array: ");
+            int numero = scanner.nextInt();
+            array[i] = numero;
+        }
+
+        for (int item : array){
+            System.out.print(item + " ");
+        }
+
+        System.out.println();
+
+        int ultPos = array[array.length - 1];
+
+        for (int i = array.length - 1; i > 0; i--) {
+            array[i] = array[i - 1];
+        }
+        array[0] = ultPos;
+
+        for (int item : array){
+            System.out.print(item + " ");
+        }
+    }
 
 }
