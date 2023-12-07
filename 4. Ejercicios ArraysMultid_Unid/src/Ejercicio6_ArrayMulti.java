205c3b90940e157ed138f7e80bf72d215b8b6d72
chitodreco <148772347+chitodreco@users.noreply.github.com>
2023-12-07 21:35:54 +0100

    update

---
+++ 4. Ejercicios ArraysMultid_Unid/src/Ejercicio6_ArrayMulti.java
@@ -0,0 +1,15 @@
+//1. (GeneradorPalabras) Crea un programa que cumpla los siguientes requisitos:
+//        - Crear un de chars con todas las letras del abecedario.
+//        - Crear un array de String con una longitud pedida por teclado
+//        - Pedir la longitud de cada una de las palabras que formarán parte del array del apartado 2.
+//        Tras pedir la longitud se creará una palabra de forma aleatoria
+//        con la longitud deseada (cada letra será una posición del array del apartado 1)
+//        y se guardará en el array del apartado 2
+//        - Imprimir por pantalla las palabras generadas
+
+public class Ejercicio6_ArrayMulti {
+
+    public static void main(String[] args) {
+        
+    }
+}
