14b0adb439132b5313d28f3390072b0bf6d66f40
chitodreco <148772347+chitodreco@users.noreply.github.com>
2023-12-10 16:30:36 +0100

    update

---
+++ 4. Ejercicios ArraysMultid_Unid/src/Ejercicio7_ArrayMulti.java
@@ -0,0 +1,20 @@
+//1. (AhorcadoArray) Crear un array de String donde se guarden 20 palabras.
+// El sistema deberá de seleccionar una de las palabras pertenecientes al array.
+// Una vez seleccionada se jugará al juego del ahorcado donde la palabra
+// a acertar será la seleccionada. Para ello las normas son las siguientes:
+//        - Nada mas arrancar se mostrarán por consola tantos _ como letras tenga la palabra a acertar
+//        - Se pedirá por pantalla la letra con la que se quiere probar
+//        - En el caso de acertar se mostrará la letra en la posición correspondiente
+//        - En el caso de falla se restará una vida
+//        - El programa continuará hasta que:
+//        - Acierte la palabra
+//        - Me quede sin vidas
+
+public class Ejercicio7_ArrayMulti {
+
+    public static void main(String[] args) {
+
+        String[] palabras = new String[]{"casa", "perro", "movil", "tele", "lapiz"};
+        
+    }
+}
