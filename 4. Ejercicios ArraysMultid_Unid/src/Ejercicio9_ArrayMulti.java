0c23dbb3ced8d61483627f8cb2846d5a8e3d41cf
chitodreco <148772347+chitodreco@users.noreply.github.com>
2023-12-11 18:46:45 +0100

    update

---
+++ 4. Ejercicios ArraysMultid_Unid/src/Ejercicio9_ArrayMulti.java
@@ -0,0 +1,18 @@
+//1. (MenuArray)Crear un programa que mediante un menú permita:
+//        - Crear un array de números enteros con n posiciones pedidas.
+//        - Rellenar el array creado con números aleatorios.
+//        - Rellenar el array creado con números pedidos por consola.
+//        - Ordenar el array de mayor a menor
+//        - Clonar el array con una una mayor longitud. Para ello pedir al usuario la nueva longitud
+//        (si esta es inferior a la que la existe continuar pidiendo)
+//        - Mover todas las posiciones (pedir al usuario que seleccione la orientación)
+//        - Mostrar por pantalla el array según está
+
+public class Ejercicio9_ArrayMulti {
+
+    public static void main(String[] args) {
+
+        
+
+    }
+}
