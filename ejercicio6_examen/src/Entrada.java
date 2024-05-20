7e0913b5bfa357ab1bc318326156d1ecad632708
chitodreco <148772347+chitodreco@users.noreply.github.com>
2024-05-20 21:37:49 +0200

    update

---
+++ ejercicio6_examen/src/Entrada.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class Entrada {
+
+    public static void main(String[] args) {
+        ArrayList<String[]> estudiantes = new ArrayList<>();
+        File file = new File("src/resources/alumno.txt");
+        FileReader fr = null;
+        BufferedReader bf = null;
+
+        try {
+            fr = new FileReader(file);
+            bf = new BufferedReader(fr);
+            String linea = null;
+
+            while ((linea = bf.readLine()) != null) {
+
+                String[] datos = linea.split(", ");
+                estudiantes.add(datos);
+                for (int i = 0; i < datos.length; i++) {
+
+                    System.out.println(datos[i]);
+
+                }
+
+            }
+        }catch (FileNotFoundException e){
+            System.out.println("Fichero no existe");
+        }catch (IOException e){
+            System.out.println("No se puede leer");
+        } finally {
+            try {
+                bf.close();
+                fr.close();
+            } catch (IOException e) {
+                System.out.println("Error en cerrado");
+            } catch (NullPointerException e){
+                System.out.println("Cerrado en nulo");
+            }
+        }
+
+
+    }
+
+}
