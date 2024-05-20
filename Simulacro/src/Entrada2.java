f8bb31effc70e57fa9842c287dc8c280bef85b52
chitodreco <148772347+chitodreco@users.noreply.github.com>
2024-05-20 10:19:54 +0200

    update

---
+++ Simulacro/src/Entrada2.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Entrada2 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int contador = 0;
+        while (contador != 5) {
+            System.out.println("Introduzca un número del 1 al 10: ");
+            int numero = scanner.nextInt();
+            contador++;
+            ArrayList<numeros> = numero.add
+        }
+    }
+
+}
