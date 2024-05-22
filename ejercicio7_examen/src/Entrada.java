8e582a20c1183ffe1e62d8915a7252653888dadf
chitodreco <148772347+chitodreco@users.noreply.github.com>
2024-05-22 19:06:42 +0200

    update

--- ejercicio7_examen/src/Entrada.java
+++ ejercicio7_examen/src/Entrada.java
@@ -11,7 +11,7 @@ public class Entrada {
 
         if(palabra.length() < 4){
             System.out.println(palabra.toUpperCase());
-        } else if (palabra.length()<8) {
+        } else if (palabra.length() < 8) {
             System.out.println(palabra.toLowerCase());            
         } else if (palabra.length() < 11) {
             palabra = palabra.replaceAll("a","á")
