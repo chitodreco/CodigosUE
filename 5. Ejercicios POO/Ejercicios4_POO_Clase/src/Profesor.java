bfd25c16478726fd3d0adde6c404e9f9a07cf102
chitodreco <148772347+chitodreco@users.noreply.github.com>
2024-05-26 20:49:54 +0200

    update

--- 5. Ejercicios POO/Ejercicios4_POO_Clase/src/Profesor.java
+++ 5. Ejercicios POO/Ejercicios4_POO_Clase/src/Profesor.java
@@ -8,7 +8,15 @@
 public class Profesor {
 
     public void ponerNotas(Alumno alumno){
-        
+        alumno.getAsignatura().setCalificacion(Math.random()*11);
+        alumno.getAsigntura2().setCalificacion(Math.random()*11);
+        alumno.getAsignatura3().setCalificacion(Math.random()*11);
+    }
+
+    public double calcularMedia(Alumno alumno){
+        double media = ((alumno.getAsignatura3().getCalificacion() + alumno.getAsignatura().getCalificacion()
+                + alumno.getAsigntura2().getCalificacion()) / 3);
+        return media;
     }
 
 }
