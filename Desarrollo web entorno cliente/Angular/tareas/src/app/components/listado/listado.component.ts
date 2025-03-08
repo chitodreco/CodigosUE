1d097d1637d7f9563189531f59d2da571824d10c
chitodreco <roberto.panizo.ateza@gmail.com>
2025-03-08 11:30:36 +0100

    update

--- Desarrollo web entorno cliente/Angular/tareas/src/app/components/listado/listado.component.ts
+++ Desarrollo web entorno cliente/Angular/tareas/src/app/components/listado/listado.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Conocimiento } from '../../model/modelos';
 
 @Component({
   selector: 'app-listado',
@@ -10,10 +11,22 @@ export class ListadoComponent {
   nombre = 'Roberto';
   nombreConocimiento = '';
   nivelConocimiento = '';
+  conocimientos: Conocimiento[] = []; /* declara la variable conocimiento, de tipo interfaz como array vacío. Luego la función lo rellenará */
+
   url = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwhFNGv9UvSO3RW0hpy2WCoZ1Q2PJ1zmqHZg&s';
 
   guardarElemento(){
-    this.nombreConocimiento = nombreConocimiento;
-    this.nivelConocimiento = nivelConocimiento;    
+
+
+  let conocimiento: Conocimiento = {nombre:this.nombreConocimiento, nivel:this.nivelConocimiento} /* dentro de la función guardarElemento declara variable conomiento, que es de tipo interfaz Conocimiento 
+  y guarda las variables introducidas en el html como tipo JSON -->atributo:valor*/
+
+    this.conocimientos.push(conocimiento); /* desde aquí se meterán en el array los conocimientos */
+
+
+    console.log(this.nombreConocimiento);
+    ;
+    console.log(this.nivelConocimiento);    
+    ;
   }
 }
